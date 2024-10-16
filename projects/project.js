5a7981afa38ed251d16b334e6a3b508da9345316
Harish T <142471897+harishthirumalai18@users.noreply.github.com>
2024-10-16 22:15:32 +0530

    Add files via upload

---
+++ projects/project.js
@@ -0,0 +1,239 @@
+// import coschedule from "../images/coschedule.png";
+const Projects = [
+  // {
+  //   name: "PromptWiz: Your Creative Prompt Hub",
+  //   about:
+  //     "PromptWiz is a full-stack open-source prompt tool for the modern world. This project allows users to discover, create, share, copy, edit and delete creative prompts.",
+  //   img: "./images/promptwiz.png",
+  //   gitrepo: "https://github.com/Divya-Konala/PromptWiz",
+  //   tech_stack: "Next.js | MongoDB | Tailwind CSS",
+  //   tech_stackicons: [
+  //     `<img src="https://img.icons8.com/fluency-systems-regular/48/nextjs.png"/>`,
+  //     `<img src="https://img.icons8.com/color/50/000000/mongodb.png"/>`,
+  //     `<img src="https://img.icons8.com/color/48/tailwindcss.png"/>`,
+  //   ],
+  //   project_link: "https://prompt-wiz-pi.vercel.app",
+  //   project_type: "Individual Project",
+  // },
+  {
+    name: "BhuviEtextiles",
+    about: "Bhuvietextiles is a web project developed using HTML, CSS, JavaScript, and React, designed to showcase and manage textile products.",
+    img: "./images/bhuvietextiles.png",
+    tech_stack: "HTML | CSS | JS | ReactJs| SQL",
+    gitrepo: "https://github.com/harishthirumalai18/BhuviEtextiles",
+    tech_stackicons: [
+      `<img src="https://img.icons8.com/color/48/000000/html-5"/>`,
+      `<img src="https://img.icons8.com/color/48/000000/css3"/>`,
+      `<img src="https://img.icons8.com/color/48/000000/javascript--v2.png"/>`,
+    `<img src="https://img.icons8.com/office/48/react.png"/>`,
+    //  `<img src="https://img.icons8.com/color/48/material-ui.png"/>`,
+     // `<img src="https://img.icons8.com/color/48/sass.png"/>`,
+      `<img src="https://img.icons8.com/office/48/sql.png"/>`,
+    ],
+    project_link: "https://bhuvietextiles.netlify.app/",
+    project_type: "Individual Project",
+  },
+  {
+    name: "Animal Rescue System",
+    about:
+      "The Animal Rescue System is a web-based platform designed to streamline the reporting, rescue, and rehabilitation of animals in distress, utilizing technologies like HTML, CSS, JavaScript, React JS, and MySQL.",
+    img: "./images/animalrescue.png",
+    gitrepo: "https://github.com/harishthirumalai18/Animalrescue",
+    tech_stack: "HTML | CSS | JS | SQL",
+    tech_stackicons: [
+      `<img src="https://img.icons8.com/color/48/000000/html-5"/>`,
+      `<img src="https://img.icons8.com/color/48/000000/css3"/>`,
+      `<img src="https://img.icons8.com/color/48/000000/javascript--v2.png"/>`,
+      '<img src="https://img.icons8.com/office/48/sql.png"/>',
+          ],
+    project_link: "animalrescuevtht.netlify.app",
+    project_type: "Individual Project",
+  },
+ 
+  {
+    name: "Employee Time Tracker Application",
+    about:
+      "The Employee Time Tracker is a web application designed to monitor and manage employee work hours, attendance, and productivity, utilizing Java and JSP pages for backend functionality and user interface development.",
+    img: "./images/tasktracker.jpg",
+    gitrepo: "https://github.com/harishthirumalai18/Employee-time-Tracker-Project2-Genpact",
+    tech_stack: "Java| HTML | CSS | APACHE TOMCAT | SQL ",
+    tech_stackicons: [
+      `<img src="https://img.icons8.com/color/48/000000/html-5"/>`,
+      `<img src="https://img.icons8.com/color/48/000000/css3"/>`,
+      `<img src="https://img.icons8.com/color/48/java-coffee-cup-logo--v1.png"/>`,
+      `<img src="https://img.icons8.com/?size=48&id=QFcVqyh6lBh6&format=png"/>`,
+      `<img src="https://img.icons8.com/office/48/sql.png"/>`
+      
+    ],
+    project_link: "",
+    project_type: "Individual Project",
+  },
+
+  {
+    name: "Banking Application",
+    about:
+      "The Banking Application is a web-based financial management system developed using Java Servlet pages in Eclipse, designed for secure transactions and account management.",
+    img: "./images/bankingapp.jpg",
+    gitrepo: "https://github.com/harishthirumalai18/BankingApplication-Project1-Genpact",
+    tech_stack: "Java| HTML | CSS | APACHE TOMCAT | SQL ",
+    tech_stackicons: [
+      `<img src="https://img.icons8.com/color/48/000000/html-5"/>`,
+      `<img src="https://img.icons8.com/color/48/000000/css3"/>`,
+      `<img src="https://img.icons8.com/color/48/java-coffee-cup-logo--v1.png"/>`,
+      `<img src="https://img.icons8.com/?size=48&id=QFcVqyh6lBh6&format=png"/>`,
+      `<img src="https://img.icons8.com/office/48/sql.png"/>`,
+    ],
+    project_link: "",
+    project_type: "Individual Project",
+  },
+  {
+    name: "Clone-Stamptool",
+    about: "StampTool is a web application for managing and generating digital stamps, developed using HTML, CSS, JavaScript, and API, and deployed via Vercel.",
+    img: "./images/clonestamp.png",
+    tech_stack: "HTML | CSS | JS ",
+    gitrepo: "https://github.com/harishthirumalai18/stamptool",
+    tech_stackicons: [
+      `<img src="https://img.icons8.com/color/48/000000/html-5"/>`,
+      `<img src="https://img.icons8.com/color/48/000000/css3"/>`,
+      `<img src="https://img.icons8.com/color/48/000000/javascript--v2.png"/>`,
+     // `<img src="https://img.icons8.com/office/48/react.png"/>`,
+    //  `<img src="https://img.icons8.com/color/48/material-ui.png"/>`,
+    //  `<img src="https://img.icons8.com/color/48/sass.png"/>`,
+     // `<img src="https://img.icons8.com/office/48/sql.png"/>`,
+    ],
+    project_link: "https://stamptool.vercel.app/",
+    project_type: "Team Project",
+  },
+  {
+    name: "Quick-Selection-Tool",
+    about: "The Quick-Selection-Tool is a web application developed using HTML, CSS, JavaScript, and API, designed for efficient selection and manipulation of elements. It was created during your Creatvise internship and is deployed using Vercel for seamless access.",
+    img: "./images/Quickselectiontool.png",
+    tech_stack: "HTML | CSS | JS ",
+    gitrepo: "",
+    tech_stackicons: [
+      `<img src="https://img.icons8.com/color/48/000000/html-5"/>`,
+      `<img src="https://img.icons8.com/color/48/000000/css3"/>`,
+      `<img src="https://img.icons8.com/color/48/000000/javascript--v2.png"/>`,
+   //   `<img src="https://img.icons8.com/office/48/react.png"/>`,
+   //   `<img src="https://img.icons8.com/color/48/material-ui.png"/>`,
+     // `<img src="https://img.icons8.com/color/48/sass.png"/>`,
+    //  `<img src="https://img.icons8.com/office/48/sql.png"/>`,
+    ],
+    project_link: "https:creatvise.vercel.app/",
+    project_type: "Team Project",
+  },
+  {
+    name: "Expense Tracker",
+    about: "The expense tracker is a web application designed to help users track and manage their daily expenses efficiently.",
+    img: "./images/expensetracker.png",
+    tech_stack: "HTML | CSS | JS | REACT",
+    gitrepo: "https://github.com/harishthirumalai18/Expense-Tracker",
+    tech_stackicons: [
+      `<img src="https://img.icons8.com/color/48/000000/html-5"/>`,
+      `<img src="https://img.icons8.com/color/48/000000/css3"/>`,
+      `<img src="https://img.icons8.com/color/48/000000/javascript--v2.png"/>`,
+     `<img src="https://img.icons8.com/office/48/react.png"/>`
+   //   `<img src="https://img.icons8.com/color/48/material-ui.png"/>`,
+     // `<img src="https://img.icons8.com/color/48/sass.png"/>`,
+    //  `<img src="https://img.icons8.com/office/48/sql.png"/>`,
+    ],
+    project_link: "https:harishtexpensetracker.netlify.app/",
+    project_type: "Own Project",
+  },
+  {
+    name: "Portfolio",
+    about: "The portfolio project showcases my personal work and skills through an interactive and responsive website with a contact form for inquiries. Tools used include HTML, CSS, and JavaScript for the frontend design and functionality.",
+    img: "./images/portfolio.png",
+    tech_stack: "HTML | CSS | JS ",
+    gitrepo: "https://github.com/harishthirumalai18",
+    tech_stackicons: [
+      `<img src="https://img.icons8.com/color/48/000000/html-5"/>`,
+      `<img src="https://img.icons8.com/color/48/000000/css3"/>`,
+      `<img src="https://img.icons8.com/color/48/000000/javascript--v2.png"/>`,
+     `<img src="https://img.icons8.com/office/48/react.png"/>`
+   //   `<img src="https://img.icons8.com/color/48/material-ui.png"/>`,
+     // `<img src="https://img.icons8.com/color/48/sass.png"/>`,
+    //  `<img src="https://img.icons8.com/office/48/sql.png"/>`,
+    ],
+    project_link: "https:harishtportfolio.netlify.app/",
+    project_type: "Own Project",
+  },
+  {
+    name: "Notes-App",
+    about: "A simple Notes app built using React that allows users to create, edit, and delete notes efficiently, providing an easy way to manage personal or work-related information.",
+    img: "./images/notes.png",
+    tech_stack: "HTML | CSS | JS | REACT",
+    gitrepo: "https://github.com/harishthirumalai18/notes",
+    tech_stackicons: [
+      `<img src="https://img.icons8.com/color/48/000000/html-5"/>`,
+      `<img src="https://img.icons8.com/color/48/000000/css3"/>`,
+      `<img src="https://img.icons8.com/color/48/000000/javascript--v2.png"/>`,
+     `<img src="https://img.icons8.com/office/48/react.png"/>`
+   //   `<img src="https://img.icons8.com/color/48/material-ui.png"/>`,
+     // `<img src="https://img.icons8.com/color/48/sass.png"/>`,
+    //  `<img src="https://img.icons8.com/office/48/sql.png"/>`,
+    ],
+    project_link: "https:harishtnotes.netlify.app/",
+    project_type: "Own Project",
+  },
+  {
+    name: "Resume",
+    about: "A simple Resume.",
+    img: "./images/resumeonline.png",
+    tech_stack: "HTML | CSS | JS ",
+    gitrepo: "https://github.com/harishthirumalai18/harishtresume",
+    tech_stackicons: [
+      `<img src="https://img.icons8.com/color/48/000000/html-5"/>`,
+      `<img src="https://img.icons8.com/color/48/000000/css3"/>`,
+      `<img src="https://img.icons8.com/color/48/000000/javascript--v2.png"/>`,
+    // `<img src="https://img.icons8.com/office/48/react.png"/>`
+   //   `<img src="https://img.icons8.com/color/48/material-ui.png"/>`,
+     // `<img src="https://img.icons8.com/color/48/sass.png"/>`,
+    //  `<img src="https://img.icons8.com/office/48/sql.png"/>`,
+    ],
+    project_link: "https:harishtresume.vercel.app/",
+    project_type: "Own Project",
+  },
+  
+];
+
+let projects__container = document.getElementById("projects--container");
+
+Projects.forEach((pro) => {
+  let main = document.createElement("div");
+  main.setAttribute("class", "portfolio-item padd-15");
+  main.innerHTML = `
+    <div class="portfolio-item-inner shadow-dark">
+    <div class="portfolio-img">
+      <img src=${pro.img} alt="photo" class="pro-i">
+      <div class="project-desc"> <h1 class="project-name"><b>${
+        pro.name
+      }</b></h1> <br/>
+      <h4 class="project-name project__about">${pro.about}</h4>
+      <div class="navigate_ar">
+      <button> <a href=${
+        pro.project_link
+      } target="_blank" class="see-project">LIVE</a></button>
+      <button> <a href=${
+        pro.gitrepo 
+      } target="_blank" class="github-repo">GitHub💻</a> </button>
+      
+     
+      </div>
+    
+      <h3 class="project-name techcen"><b>Tech Stack</b></h3>
+      <span >${pro.tech_stackicons[0] ? pro.tech_stackicons[0] : ""}
+      ${pro.tech_stackicons[1] ? pro.tech_stackicons[1] : ""}
+      ${pro.tech_stackicons[2] ? pro.tech_stackicons[2] : ""}
+      ${pro.tech_stackicons[3] ? pro.tech_stackicons[3] : ""}
+      ${pro.tech_stackicons[4] ? pro.tech_stackicons[4] : ""}
+      ${pro.tech_stackicons[5] ? pro.tech_stackicons[5] : ""}
+      ${pro.tech_stackicons[6] ? pro.tech_stackicons[6] : ""}
+      </span> </div>
+     
+      
+    </div>
+  </div>
+    `;
+  projects__container.append(main);
+});
